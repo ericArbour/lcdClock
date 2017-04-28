7455b43191ef4f4885f3d024332ff79037f54a3c
Eric Arbour <arboureric@gmail.com>
2017-04-28 18:59:19 -0400

    werkinonit

--- lcd.js
+++ lcd.js
@@ -1,3 +1,45 @@
+function timeMaker (num) {
+ var strings = [
+  [[' ', '_', ' '], ['|', ' ', '|'], ['|', '_', '|']],
+  [[' ', ' ', ' '], [' ', ' ', '|'], [' ', ' ', '|']],
+  [[' ', '_', ' '], [' ', '_', '|'], ['|', '_', ' ']],
+  [[' ', '_', ' '], [' ', '_', '|'], [' ', '_', '|']],
+  [[' ', ' ', ' '], ['|', '_', '|'], [' ', ' ', '|']],
+  [[' ', '_', ' '], ['|', '_', ' '], [' ', '_', '|']],
+  [[' ', '_', ' '], ['|', '_', ' '], ['|', '_', '|']],
+  [['_', '_', ' '], [' ', '/', ' '], ['/', ' ', ' ']],
+  [[' ', '_', ' '], ['|', '_', '|'], ['|', '_', '|']],
+  [[' ', '_', ' '], ['|', '_', '|'], [' ', ' ', '|']]
+ ];
+ var numString = num.toString();
+ var result = '';
+ for (var i = 0; i < numString.length; i++) {
+  var line = [];
+  for (var j = 0; j < numString.length; j++) {
+   if (j === 0) {
+    line = [line.concat(strings[Number(numString[j])][i]).join('')];
+   } else if (j === 2 && (i === 1 || i === 2)) {
+    line = [line.concat(' . ',  strings[Number(numString[j])][i]).join('')];
+   } else if (j === 2) {
+    line = [line.concat('   ',  strings[Number(numString[j])][i]).join('')];
+   } else {
+    line = [line.concat(' ',  strings[Number(numString[j])][i]).join('')];
+   }
+  }
+  console.log(line);
+  result += line + '\n';
+ }
+ return result;
+}
+
+console.log(timeMaker(65522));
+setInterval(function() {
+ var time = new Date();
+ console.log(time.getHours());
+ console.log(time.getMinutes());
+ console.log(time.getSeconds());
+}, 1000000);
+
 function lcd (num) {
  var strings = [
   [[' ', '_', ' '], ['|', ' ', '|'], ['|', '_', '|']],
@@ -22,4 +64,3 @@ function lcd (num) {
  }
  return result;
 }
-console.log(lcd(1234567890));
